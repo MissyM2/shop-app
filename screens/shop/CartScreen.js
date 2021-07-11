beaee2c6e59882f674e94b4a9fae136e6d5bf09d
Missy Maloney <missymaloney1@gmail.com>
2021-07-11 16:24:04 -0400

    added authscreen:  code is there but data is no longer hitting server: user, products or orders

--- screens/shop/CartScreen.js
+++ screens/shop/CartScreen.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import { View, Text, FlatList, Button, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  FlatList,
+  Button,
+  StyleSheet,
+  ActivityIndicator
+} from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
 
 import Colors from '../../constants/Colors';
 import CartItem from '../../components/shop/CartItem';
+import Card from '../../components/UI/Card';
 import * as cartActions from '../../store/actions/cart';
 import * as ordersActions from '../../store/actions/orders';
-import Card from '../../components/UI/Card';
 
 const CartScreen = props => {
+  const [isLoading, setIsLoading] = useState(false);
+
   const cartTotalAmount = useSelector(state => state.cart.totalAmount);
   const cartItems = useSelector(state => {
     const transformedCartItems = [];
@@ -27,22 +36,32 @@ const CartScreen = props => {
   });
   const dispatch = useDispatch();
 
+  const sendOrderHandler = async () => {
+    setIsLoading(true);
+    await dispatch(ordersActions.addOrder(cartItems, cartTotalAmount));
+    setIsLoading(false);
+  };
+
   return (
-    <Card style={styles.screen}>
-      <View style={styles.summary}>
+    <View style={styles.screen}>
+      <Card style={styles.summary}>
         <Text style={styles.summaryText}>
           Total:{' '}
-          <Text style={styles.amount}>${Math.round(cartTotalAmount.toFixed(2) * 100) / 100}</Text>
+          <Text style={styles.amount}>
+            ${Math.round(cartTotalAmount.toFixed(2) * 100) / 100}
+          </Text>
         </Text>
-        <Button
-          color={Colors.accent}
-          title="Order Now"
-          disabled={cartItems.length === 0}
-          onPress={() => {
-            dispatch(ordersActions.addOrder(cartItems, cartTotalAmount));
-          }}
-        />
-      </View>
+        {isLoading ? (
+          <ActivityIndicator size="small" color={Colors.primary} />
+        ) : (
+          <Button
+            color={Colors.accent}
+            title="Order Now"
+            disabled={cartItems.length === 0}
+            onPress={sendOrderHandler}
+          />
+        )}
+      </Card>
       <FlatList
         data={cartItems}
         keyExtractor={item => item.productId}
@@ -58,7 +77,7 @@ const CartScreen = props => {
           />
         )}
       />
-    </Card>
+    </View>
   );
 };
 
--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect, useCallback } from 'react';
-import { 
-    View,
-    Text,
-    FlatList, 
-    Button, 
-    Platform, 
-    ActivityIndicator, 
-    StyleSheet 
+import {
+  View,
+  Text,
+  FlatList,
+  Button,
+  Platform,
+  ActivityIndicator,
+  StyleSheet
 } from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
@@ -16,77 +16,81 @@ import ProductItem from '../../components/shop/ProductItem';
 import * as cartActions from '../../store/actions/cart';
 import * as productsActions from '../../store/actions/products';
 import Colors from '../../constants/Colors';
-import { isLoading } from 'expo-font';
+//import { isLoading } from 'expo-font';
 
 
 const ProductsOverviewScreen = props => {
     const [isLoading, setIsLoading] = useState(false);
+    const [isRefreshing, setIsRefreshing] = useState(false);
     const [error, setError] = useState();
     const products = useSelector(state => state.products.availableProducts);
     const dispatch = useDispatch();
-
+  
     const loadProducts = useCallback(async () => {
-        setError(null);
-        setIsLoading(true);
-        try{
-            await dispatch(productsActions.fetchProducts());
-        } catch (err) {
-            setError(err.message);
-        }
-        setIsLoading(false);
+      setError(null);
+      setIsRefreshing(true);
+      try {
+        await dispatch(productsActions.fetchProducts());
+      } catch (err) {
+        setError(err.message);
+      }
+      setIsRefreshing(false);
     }, [dispatch, setIsLoading, setError]);
-
-    // drawer navigation is "capped" - it does not refetch from server
-    //  Adding a listener here, listens to any changes and triggers a refetch 
+  
     useEffect(() => {
-        const willFocusSubscription = props.navigation.addListener('willFocus', loadProducts);
-        
-        // this is a cleanup function that removes the listener after the component
-        // is destroyed.  We have not used a return with useEffect before but you can do it.
-        return () => {
-            willFocusSubscripton.remove();
-        };
+      const willFocusSub = props.navigation.addListener(
+        'willFocus',
+        loadProducts
+      );
+  
+      return () => {
+        willFocusSub.remove();
+      };
     }, [loadProducts]);
-
+  
     useEffect(() => {
-        loadProducts();   
+      setIsLoading(true);
+      loadProducts().then(() => {
+        setIsLoading(false);
+      });
     }, [dispatch, loadProducts]);
-
+  
     const selectItemHandler = (id, title) => {
-        props.navigation.navigate('ProductDetail', { 
-            productId: id,
-            productTitle: title
-        });
-
+      props.navigation.navigate('ProductDetail', {
+        productId: id,
+        productTitle: title
+      });
     };
-
+  
     if (error) {
-        return (
-            <View style={styles.centered}>
-                <Text>An error occurred!</Text>
-                <Button 
-                    title="try again" 
-                    onPress={loadProducts} 
-                    color={Colors.primary} />
-            </View>
-        );
+      return (
+        <View style={styles.centered}>
+          <Text>An error occurred!</Text>
+          <Button
+            title="Try again"
+            onPress={loadProducts}
+            color={Colors.primary}
+          />
+        </View>
+      );
     }
-
+  
     if (isLoading) {
-        return (
-            <View style={styles.centered}>
-                <ActivityIndicator size='large' color={Colors.primary} />
-            </View>
-        );
+      return (
+        <View style={styles.centered}>
+          <ActivityIndicator size="large" color={Colors.primary} />
+        </View>
+      );
     }
-
+  
     if (!isLoading && products.length === 0) {
-        return (
-            <View style={styles.centered}>
-                <Text>No products found. Maybe add some?</Text>
-            </View>
-        );
+      return (
+        <View style={styles.centered}>
+          <Text>No products found. Maybe start adding some!</Text>
+        </View>
+      );
     }
+  
 
     return (
         <FlatList 
@@ -150,11 +154,7 @@ ProductsOverviewScreen.navigationOptions = navData => {
 };
 
 const styles = StyleSheet.create({
-    centered:{
-        flex: 1, 
-        justifyContent: 'center', 
-        alignItems: 'center' 
-    }
-})
+    centered: { flex: 1, justifyContent: 'center', alignItems: 'center' }
+  });
 
 export default ProductsOverviewScreen;
